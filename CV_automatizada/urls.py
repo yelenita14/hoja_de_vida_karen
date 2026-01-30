45ddab4c6ffad0636801593192edf06aa43441e6
yelenita14 <anchundiakaren005@gmail.com>
2026-01-30 02:23:41 -0500

    proyecto

--- CV_automatizada/urls.py
+++ CV_automatizada/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CV_automatizada project.
+URL configuration for hojavida_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,6 +25,7 @@ urlpatterns = [
     path('', include('hojavida.urls')),
 ]
 
-# Servir archivos media en desarrollo
+# Para servir archivos media en desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
